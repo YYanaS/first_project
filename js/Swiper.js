e98f98e73b79dec6157e5971975129f72e2b0033
Yana <yanayazuchno@gmail.com>
2024-08-10 15:32:22 +0300

    fix footer, slider & some different problems

--- js/Swiper.js
+++ js/Swiper.js
@@ -27,4 +27,33 @@ const swiper = new Swiper('.swiper', {
             spaceBetween: 20,
         }
     }
-  });
+});
+
+
+const swiperThumbs = new Swiper ('.thumbs__slider', {
+    loop: true,
+
+    spaceBetween: 40,
+    slidesPerView: 4,
+    freeMode: true,
+    loopedSlides: 5, //looped slides should be the same
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+});
+
+const swiper1 = new Swiper ('.main__slider', {
+    // Optional parameters
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    thumbs: {
+        swiper: swiperThumbs,
+    }
+
+});
+
